8fc4d774f1ba68b397a2ceccf2dab214bf45a359
liwenlin664477 <u1327012@umail.utah.edu>
2023-07-13 01:33:36 -0600

    Updated the projects

--- IFHoGP/deploy_cuda_4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-bash test_all_folds.sh burgers ifc_ode 5 5000
-
-bash test_all_folds.sh burgers ifc_ode 10 5000
-
-bash test_all_folds.sh burgers ifc_ode 20 5000
-
--- IFHoGP/deploy_cuda_4_v2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bash test_all_folds.sh burgers ifc_ode 20 5000
-
