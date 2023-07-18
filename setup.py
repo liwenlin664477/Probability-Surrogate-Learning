1eac20c3bd2268b6572ac3e467915a5d3221b37c
liwenlin664477 <u1327012@umail.utah.edu>
2023-07-18 16:46:06 -0600

    Set up the pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Probability-Surrogate-Learning",
+    version="1.0",
+    packages=find_packages(),
+    author="Wenlin Li",
+    author_email="u1327012@example.com",
+    description="This is a comprehensive machine learning library, "
+                "specifically tailored for surrogate learning and active learning.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/liwenlin664477/Probability-Surrogate-Learning",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
